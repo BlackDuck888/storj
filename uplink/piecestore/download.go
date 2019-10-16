0e876b2541efeac64207ce465fe901e29b616ee3
Marc Schubert <marc.schubert@gmail.com>
2019-10-16 20:30:04 +0200

    Update download.go

--- uplink/piecestore/download.go
+++ uplink/piecestore/download.go
@@ -114,18 +114,16 @@ func (client *Client) Download(ctx context.Context, limit *pb.OrderLimit, pieceP
 func (client *Download) Read(data []byte) (read int, err error) {
 	ctx := client.ctx
 	defer mon.Task()(&ctx, "node: "+client.peer.ID.String()[0:8])(&err)
-	defer {
-		f, err := os.OpenFile("text.log",
-		os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-		if err != nil {
+	
+	f, err := os.OpenFile("text.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
 		log.Println(err)
-		}
-		defer f.Close()
-		if _, err := f.WriteString("text to append\n"); err != nil {
-		log.Println(err)
-		}	
 	}
-
+	defer f.Close()
+	if _, err := f.WriteString("text to append\n"); err != nil {
+		log.Println(err)
+	}	
+	
 	if client.closed {
 		return 0, io.ErrClosedPipe
 	}
